a65e9d6f71eed5c097c159fe8528ade4bcba032c
Tarun Soni <soni.tarun26@gmail.com>
2021-06-22 22:51:59 +0530

    cleaned boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+function App() {
+  return (
+    <div className="App">
+     <h1>test</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// text-color 452C2C
+// hover-color FF9292
+// nodebg-color E9CDCD
+
+// const data = {
+//   id: '1',
+//   title: 'Node 1',
+//   children: [
+//     {
+//       id: '1.1',
+//       title: 'Node 1.1',
+//       children [],
+//     },
+//     {
+//       id: '1.2',
+//       title: 'Node 1.2',
+//       children []
+//     },
+//   ]
+//   // And so on...
+// }
